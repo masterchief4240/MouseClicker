8f266b7f2d7e130c3475ceabd8e4c85810ade77f
masterchief4240 <75056191+masterchief4240@users.noreply.github.com>
2020-11-26 05:15:33 +0100

    Add files via upload

---
+++ MouseClicker/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace MouseClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(135, 155);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clicking Interval";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Clicking at a specific time (HH:MM:SS)";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(34, 101);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Clicking at a specific time",
+            "Clicking Interval"});
+            this.comboBox1.Location = new System.Drawing.Point(34, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(88, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Double Click";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Enabled = true;
+            this.timer5.Interval = 10;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(34, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 45);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Milliseconds",
+            "Seconds",
+            "Minutes",
+            "Hours"});
+            this.comboBox2.Location = new System.Drawing.Point(34, 155);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(87, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 321);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "MouseClicker";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ MouseClicker/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClicker
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int clickingInterval;
+
+        public void StartIntervalClicking()
+        {
+            clickingInterval = Convert.ToInt32(numericUpDown1.Value);
+            timer1.Start();
+        }
+
+        public void StartClickingAtSpecificTime()
+        {
+            timer2.Start();
+        }
+        public void StartIntervalDoubleClicking()
+        {
+            clickingInterval = Convert.ToInt32(numericUpDown1.Value);
+            timer3.Start();
+        }
+
+        public void StartDoubleClickingAtSpecificTime()
+        {
+            timer4.Start();
+        }
+
+        //Start Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( (button1.Text == "Start") 
+                && (checkBox1.Checked == false))
+            {
+                if (comboBox1.Text == "Clicking Interval")
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    StartIntervalClicking();
+                }
+                else if (comboBox1.Text == "Clicking at a specific time")
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    StartClickingAtSpecificTime();
+                }
+            }
+            else if ((button1.Text == "Start")
+                && (checkBox1.Checked == true))
+            {
+                if (comboBox1.Text == "Clicking Interval")
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    StartIntervalDoubleClicking();
+                }
+                else if (comboBox1.Text == "Clicking at a specific time")
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    StartDoubleClickingAtSpecificTime();
+                }
+            }
+        }
+
+
+        //Timer for interval clicking
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Milliseconds")
+            {
+                timer1.Interval = clickingInterval;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+            else if (comboBox2.Text == "Seconds")
+            {
+                timer1.Interval = clickingInterval * 1000;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+            else if (comboBox2.Text == "Minutes")
+            {
+                timer1.Interval = clickingInterval * 60000;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+            else if (comboBox2.Text == "Hours")
+            {
+                timer1.Interval = clickingInterval * 3600000;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (CheckTime())
+            {
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+        }
+
+        public bool CheckTime()
+        {
+            if ((int)DateTime.Now.TimeOfDay.TotalSeconds == (int)dateTimePicker1.Value.TimeOfDay.TotalSeconds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Milliseconds")
+            {
+                timer3.Interval = clickingInterval;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+            else if (comboBox2.Text == "Seconds")
+            {
+                timer3.Interval = clickingInterval * 1000;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+            else if (comboBox2.Text == "Minutes")
+            {
+                timer3.Interval = clickingInterval * 60000;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+            else if (comboBox2.Text == "Hours")
+            {
+                timer3.Interval = clickingInterval * 3600000;
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (CheckTime())
+            {
+                int X = Convert.ToInt32(Cursor.Position.X);
+                int Y = Convert.ToInt32(Cursor.Position.Y);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)X, (uint)Y, 0, 0);
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Clicking Interval")
+            {
+                dateTimePicker1.Enabled = false;
+                numericUpDown1.Enabled = true;
+                comboBox2.Enabled = true;
+            }
+            else if (comboBox1.Text == "Clicking at a specific time")
+            {
+                dateTimePicker1.Enabled = true;
+                numericUpDown1.Enabled = false;
+                comboBox2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+}
